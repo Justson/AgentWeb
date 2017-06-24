de0611bcd0fd32a645278ab53bfc22bf574a4507
Justson <xiaozhongcen@gmail.com>
2017-06-24 10:00:15 +0800

    修复下载完成没有发送通知

--- library/src/main/java/com/just/library/RealDownLoader.java
+++ library/src/main/java/com/just/library/RealDownLoader.java
@@ -28,7 +28,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
  * source CODE  https://github.com/Justson/AgentWeb
  */
 
-public class RealDownLoader extends AsyncTask<Void, Integer, Integer> implements Observer{
+public class RealDownLoader extends AsyncTask<Void, Integer, Integer> implements Observer {
 
     private DownLoadTask mDownLoadTask;
     private long loaded = 0;
@@ -45,9 +45,9 @@ public class RealDownLoader extends AsyncTask<Void, Integer, Integer> implements
     private static final int ERROR_LOAD = 200;
 
 
-    private AtomicBoolean atomic=new AtomicBoolean(false);
+    private AtomicBoolean atomic = new AtomicBoolean(false);
 
-    private static Observable mObservable=new Observable(){
+    private static Observable mObservable = new Observable() {
         @Override
         public synchronized void setChanged() {
             super.setChanged();
@@ -100,6 +100,7 @@ public class RealDownLoader extends AsyncTask<Void, Integer, Integer> implements
     }
 
     private Exception e;
+
     @Override
     protected Integer doInBackground(Void... params) {
         int result = ERROR_LOAD;
@@ -107,15 +108,15 @@ public class RealDownLoader extends AsyncTask<Void, Integer, Integer> implements
             begin = System.currentTimeMillis();
             if (!checkDownLoaderCondition())
                 return DownLoadErrorMsg.STORAGE_ERROR.CODE;
-            if(!checknet())
+            if (!checknet())
                 return DownLoadErrorMsg.NETWORK_ERROR_CONNECTION.CODE;
             result = doDownLoad();
 
         } catch (Exception e) {
 
-            this.e=e;//逃逸
+            this.e = e;//逃逸
             LogUtils.i("Info", "doInBackground   Exception:" + e.getMessage());
-            e.printStackTrace();
+            // e.printStackTrace();
 
         }
 
@@ -172,8 +173,8 @@ public class RealDownLoader extends AsyncTask<Void, Integer, Integer> implements
         long c = System.currentTimeMillis();
         if (mNotity != null && c - time > 800) {
             time = c;
-            if(!mNotity.hasDeleteContent())
-                mNotity.setDelecte(buildCancelContent(mDownLoadTask.getContext().getApplicationContext(),mDownLoadTask.getId()));
+            if (!mNotity.hasDeleteContent())
+                mNotity.setDelecte(buildCancelContent(mDownLoadTask.getContext().getApplicationContext(), mDownLoadTask.getId()));
             mNotity.setProgress(100, (int) ((tmp + loaded) / Float.valueOf(totals) * 100), false);
         }
     }
@@ -181,7 +182,7 @@ public class RealDownLoader extends AsyncTask<Void, Integer, Integer> implements
     @Override
     protected void onPostExecute(Integer integer) {
 
-        LogUtils.i("Info","onPostExecute:"+integer);
+        LogUtils.i("Info", "onPostExecute:" + integer);
         mObservable.deleteObserver(this);
         doCallback(integer);
         if (integer > 200) {
@@ -209,17 +210,17 @@ public class RealDownLoader extends AsyncTask<Void, Integer, Integer> implements
     private void doCallback(int code) {
 
         List<DownLoadResultListener> mDownLoadResultListeners = null;
-        LogUtils.i("Info"," doCallback  mDownLoadTask.getDownLoadResultListeners():"+mDownLoadTask.getDownLoadResultListeners());
-        if (AgentWebUtils.isEmptyCollection((mDownLoadResultListeners = mDownLoadTask.getDownLoadResultListeners()))){
-            AgentWebUtils.toastShowShort(mDownLoadTask.getContext(),"下载失败出错了");
+        LogUtils.i("Info", " doCallback  mDownLoadTask.getDownLoadResultListeners():" + mDownLoadTask.getDownLoadResultListeners());
+        if (AgentWebUtils.isEmptyCollection((mDownLoadResultListeners = mDownLoadTask.getDownLoadResultListeners()))) {
+            AgentWebUtils.toastShowShort(mDownLoadTask.getContext(), "下载失败出错了");
             return;
         }
 
         for (DownLoadResultListener mDownLoadResultListener : mDownLoadResultListeners) {
 
-            if(code>200){
-                mDownLoadResultListener.error(mDownLoadTask.getFile().getAbsolutePath(),mDownLoadTask.getUrl(),DownLoadErrorMsg.getCodeToMsg(code),this.e==null?new RuntimeException("下载出错 ， 原因:"+DownLoadErrorMsg.getCodeToMsg(code)):this.e);
-            }else{
+            if (code > 200) {
+                mDownLoadResultListener.error(mDownLoadTask.getFile().getAbsolutePath(), mDownLoadTask.getUrl(), DownLoadErrorMsg.getCodeToMsg(code), this.e == null ? new RuntimeException("下载出错 ， 原因:" + DownLoadErrorMsg.getCodeToMsg(code)) : this.e);
+            } else {
 
                 mDownLoadResultListener.success(mDownLoadTask.getFile().getPath());
             }
@@ -230,30 +231,30 @@ public class RealDownLoader extends AsyncTask<Void, Integer, Integer> implements
 
     private void buildNotify(Intent intent, int id, String progressHint) {
 
-        Context mContext=mDownLoadTask.getContext().getApplicationContext();
-        if (mContext!=null&&mDownLoadTask.isEnableIndicator()) {
+        Context mContext = mDownLoadTask.getContext().getApplicationContext();
+        if (mContext != null && mDownLoadTask.isEnableIndicator()) {
 
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             PendingIntent rightPendIntent = PendingIntent.getActivity(mContext,
-                    0x33*id, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+                    0x33 * id, intent, PendingIntent.FLAG_UPDATE_CURRENT);
             int smallIcon = mDownLoadTask.getDrawableRes();
             String ticker = "您有一条新通知";
             mNotity = new Notity(mContext, id);
 
-            mNotity.notify_progress(rightPendIntent, smallIcon, ticker, "文件下载", progressHint, false, false, false, buildCancelContent(mContext,id));
+            mNotity.notify_progress(rightPendIntent, smallIcon, ticker, "文件下载", progressHint, false, false, false, buildCancelContent(mContext, id));
             mNotity.sent();
         }
     }
 
 
-    private PendingIntent buildCancelContent(Context context,int id){
+    private PendingIntent buildCancelContent(Context context, int id) {
 
         Intent intentCancel = new Intent(context, NotificationBroadcastReceiver.class);
         intentCancel.setAction("com.agentweb.notification_cancelled");
         intentCancel.putExtra("type", "type");
-        intentCancel.putExtra("TAG",mDownLoadTask.getUrl());
-        PendingIntent pendingIntentCancel = PendingIntent.getBroadcast(context, id<<3, intentCancel, PendingIntent.FLAG_UPDATE_CURRENT);
-        LogUtils.i("Info","id<<3:"+(id<<3));
+        intentCancel.putExtra("TAG", mDownLoadTask.getUrl());
+        PendingIntent pendingIntentCancel = PendingIntent.getBroadcast(context, id << 3, intentCancel, PendingIntent.FLAG_UPDATE_CURRENT);
+        LogUtils.i("Info", "id<<3:" + (id << 3));
         return pendingIntentCancel;
     }
 
@@ -269,8 +270,8 @@ public class RealDownLoader extends AsyncTask<Void, Integer, Integer> implements
             int bytes = 0;
             long previousBlockTime = -1;
 
-            boolean tag=false;
-            while (!atomic.get()&&(tag=true)) {
+
+            while (!atomic.get()) {
                 int n = bis.read(buffer, 0, 102400);
                 if (n == -1) {
                     break;
@@ -293,7 +294,8 @@ public class RealDownLoader extends AsyncTask<Void, Integer, Integer> implements
                 }
             }
 
-            if (tag) {
+            LogUtils.i("Info", "atomic:" + atomic.get() );
+            if (atomic.get()) {
                 return DownLoadErrorMsg.USER_CANCEL.CODE;
             }
             return DownLoadErrorMsg.SUCCESSFULL.CODE;
@@ -305,19 +307,18 @@ public class RealDownLoader extends AsyncTask<Void, Integer, Integer> implements
 
     }
 
-    private final void toCancel(){
+    private final void toCancel() {
         atomic.set(true);
     }
 
     @Override
     public void update(Observable o, Object arg) {
 
-        LogUtils.i("Info","update Object    ... ");
-        String url="";
-        if(arg instanceof  String&& !TextUtils.isEmpty(url= (String) arg)&&url.equals(mDownLoadTask.getUrl())){
+        LogUtils.i("Info", "update Object    ... ");
+        String url = "";
+        if (arg instanceof String && !TextUtils.isEmpty(url = (String) arg) && url.equals(mDownLoadTask.getUrl())) {
             toCancel();
         }
-
 
 
     }
@@ -341,7 +342,7 @@ public class RealDownLoader extends AsyncTask<Void, Integer, Integer> implements
 
     enum DownLoadErrorMsg {
 
-        NETWORK_ERROR_CONNECTION(400), NETWORK_ERROR_STATUS_CODE(401) , STORAGE_ERROR(402),TIME_OUT(403),USER_CANCEL(404), SUCCESSFULL(200);
+        NETWORK_ERROR_CONNECTION(400), NETWORK_ERROR_STATUS_CODE(401), STORAGE_ERROR(402), TIME_OUT(403), USER_CANCEL(404), SUCCESSFULL(200);
 
         int CODE;
 
@@ -351,7 +352,7 @@ public class RealDownLoader extends AsyncTask<Void, Integer, Integer> implements
 
 
         public static String getCodeToMsg(int code) {
-            LogUtils.i("Info",  "  CODE:" + code);
+            LogUtils.i("Info", "  CODE:" + code);
             switch (code) {
 
 
@@ -359,27 +360,27 @@ public class RealDownLoader extends AsyncTask<Void, Integer, Integer> implements
                     return "网络连接出错";
                 case 401:
                     return "连接状态码出错 ， 非200 或者 非206";
-                case 402 :
+                case 402:
                     return "内存空间不足";
                 case 403:
                     return "下载时间超时";
                 case 404:
-                    return  "用户取消下载";
+                    return "用户取消下载";
                 case 200:
                     return "下载成功";
                 default:
                     return "未知异常";
 
             }
-        };
+        }
+
+        ;
 
 
     }
 
 
-
-
-    public static class NotificationBroadcastReceiver extends BroadcastReceiver  {
+    public static class NotificationBroadcastReceiver extends BroadcastReceiver {
 
 
         public NotificationBroadcastReceiver() {
@@ -397,14 +398,14 @@ public class RealDownLoader extends AsyncTask<Void, Integer, Integer> implements
 
                 try {
 
-                    String url=intent.getStringExtra("TAG");
-                    Class<?> mClazz=mObservable.getClass();
-                    Method mMethod=mClazz.getMethod("setChanged",(Class<?>[]) null);
+                    String url = intent.getStringExtra("TAG");
+                    Class<?> mClazz = mObservable.getClass();
+                    Method mMethod = mClazz.getMethod("setChanged", (Class<?>[]) null);
                     mMethod.setAccessible(true);
-                    mMethod.invoke(mObservable,(Object[])null);
+                    mMethod.invoke(mObservable, (Object[]) null);
                     mObservable.notifyObservers(url);
-                    LogUtils.i("Info","size:"+mObservable.countObservers());
-                }catch (NoSuchMethodException e){
+                    LogUtils.i("Info", "size:" + mObservable.countObservers());
+                } catch (NoSuchMethodException e) {
                     e.printStackTrace();
                 } catch (InvocationTargetException e1) {
                     e1.printStackTrace();
@@ -417,10 +418,6 @@ public class RealDownLoader extends AsyncTask<Void, Integer, Integer> implements
 
 
     }
-
-
-
-
 
 
 }
