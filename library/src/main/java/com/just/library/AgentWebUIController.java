8b25f5e51da1a15ce4a2f6f4f366fab2c1a9191d
Justson <xiaozhongcen@gmail.com>
2017-12-08 00:31:43 +0800

    update AgentWebUIController

--- library/src/main/java/com/just/library/AgentWebUIController.java
+++ library/src/main/java/com/just/library/AgentWebUIController.java
@@ -8,18 +8,43 @@ import android.webkit.WebView;
  */
 public abstract class AgentWebUIController {
 
+    private static boolean hasDesignLib = false;
+
+    static {
+        try {
+            Class.forName("android.support.design.widget.Snackbar");
+            hasDesignLib = true;
+        } catch (Throwable ignore) {
+            hasDesignLib = false;
+        }
+    }
+
+    private AgentWebUIController mAgentWebUIControllerDelegate;
+
+    public AgentWebUIController create() {
+        return hasDesignLib ? new DefaultDesignUIController() : new DefaultUIController();
+    }
 
     protected void onJsAlert(WebView view, String url, String message) {
 
+        getDelegate().onJsAlert(view, url, message);
     }
 
 
     protected void onJsConfirm(WebView view, String url, String message) {
-
+        getDelegate().onJsConfirm(view, url, message);
     }
 
 
     public void onJsPrompt(WebView view, String url, String message, String defaultValue) {
+        getDelegate().onJsPrompt(view, url, message, defaultValue);
+    }
 
+    public AgentWebUIController getDelegate() {
+        AgentWebUIController mAgentWebUIController = this.mAgentWebUIControllerDelegate;
+        if (mAgentWebUIController == null) {
+            this.mAgentWebUIControllerDelegate = mAgentWebUIController = create();
+        }
+        return mAgentWebUIController;
     }
 }
