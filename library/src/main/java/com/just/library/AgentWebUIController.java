ebb3e8b0378bda191d86cc099dfe65679d889d8c
Justson <xiaozhongcen@gmail.com>
2017-12-08 00:25:26 +0800

    AgentWeb 的交互的UI加入自定义

--- library/src/main/java/com/just/library/AgentWebUIController.java
+++ library/src/main/java/com/just/library/AgentWebUIController.java
@@ -1,8 +1,25 @@
 package com.just.library;
 
+import android.webkit.WebView;
+
 /**
  * Created by cenxiaozhong on 2017/12/6.
+ * 该类统一控制了与用户交互的一些UI
  */
+public abstract class AgentWebUIController {
 
-public class AgentWebUIController {
+
+    protected void onJsAlert(WebView view, String url, String message) {
+
+    }
+
+
+    protected void onJsConfirm(WebView view, String url, String message) {
+
+    }
+
+
+    public void onJsPrompt(WebView view, String url, String message, String defaultValue) {
+
+    }
 }
---
+++ library/src/main/java/com/just/library/DefaultUIController.java
@@ -0,0 +1,9 @@
+package com.just.library;
+
+/**
+ * Created by cenxiaozhong on 2017/12/8.
+ */
+
+public class DefaultDesignUIController extends AgentWebUIController {
+
+}
