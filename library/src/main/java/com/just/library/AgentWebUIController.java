5b731d3eb9a15df1f0a2927c3bbc61e3fa12e915
Justson <xiaozhongcen@gmail.com>
2017-12-10 09:45:22 +0800

    update AgentWebUIController

--- library/src/main/java/com/just/library/AgentWebUIController.java
+++ library/src/main/java/com/just/library/AgentWebUIController.java
@@ -1,11 +1,14 @@
 package com.just.library;
 
 import android.app.Activity;
+import android.os.Handler;
+import android.webkit.JsPromptResult;
+import android.webkit.JsResult;
 import android.webkit.WebView;
 
 /**
  * Created by cenxiaozhong on 2017/12/6.
- * 该类统一控制了与用户交互的一些界面
+ * 该类统一控制了与用户交互的界面
  */
 public abstract class AgentWebUIController {
 
@@ -30,14 +33,6 @@ public abstract class AgentWebUIController {
         return hasDesignLib ? new DefaultDesignUIController() : new DefaultUIController();
     }
 
-    public abstract void onJsAlert(WebView view, String url, String message);
-
-
-    public abstract void onJsConfirm(WebView view, String url, String message);
-
-
-    public abstract void onJsPrompt(WebView view, String url, String message, String defaultValue);
-
     protected AgentWebUIController getDelegate() {
         AgentWebUIController mAgentWebUIController = this.mAgentWebUIControllerDelegate;
         if (mAgentWebUIController == null) {
@@ -46,8 +41,7 @@ public abstract class AgentWebUIController {
         return mAgentWebUIController;
     }
 
-    synchronized final void bindWebParent(WebParentLayout webParentLayout, Activity activity) {
-
+    final synchronized void bindWebParent(WebParentLayout webParentLayout, Activity activity) {
         if (!isBindWebParent) {
             isBindWebParent = true;
             this.mWebParentLayout = webParentLayout;
@@ -57,7 +51,16 @@ public abstract class AgentWebUIController {
 
     }
 
-    abstract void bindSupportWebParent(WebParentLayout webParentLayout, Activity activity);
+    protected abstract void bindSupportWebParent(WebParentLayout webParentLayout, Activity activity);
+
+    public abstract void onJsAlert(WebView view, String url, String message);
+
+
+    public abstract void onJsConfirm(WebView view, String url, String message, JsResult jsResult);
+
+    public abstract void showChooser(WebView view, String url, String[] ways, Handler.Callback callback);
+
+    public abstract void onJsPrompt(WebView view, String url, String message, String defaultValue, JsPromptResult jsPromptResult);
 
 
 }
