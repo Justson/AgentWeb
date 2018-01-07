e1106bd869d3fb58914e135b8b972b1c3feac7a5
Justson <xiaozhongcen@gmail.com>
2018-01-07 09:59:30 +0800

    update App

--- sample/src/main/java/com/just/agentweb/sample/app/App.java
+++ sample/src/main/java/com/just/agentweb/sample/app/App.java
@@ -15,7 +15,12 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
 
-        //提前初始化WebView
+        /**
+         * 说明， WebView 初处初始化耗时 250ms 左右。
+         * 提前初始化WebView ，好处可以提升页面初始化速度，减少白屏时间，
+         * 坏处，拖慢了App 冷启动速度，如果 WebView 配合 VasSonic 使用，
+         * 建议不要在此处提前初始化 WebView 。
+         */
 //        WebView mWebView=new WebView(new MutableContextWrapper(this));
 
         if (LeakCanary.isInAnalyzerProcess(this)) {
