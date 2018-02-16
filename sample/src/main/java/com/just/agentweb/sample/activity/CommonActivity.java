0c1ad77465c803994302e71fb2062b3f197d0dd8
Justson <xiaozhongcen@gmail.com>
2018-02-16 20:14:34 +0800

    #

--- sample/src/main/java/com/just/agentweb/sample/activity/CommonActivity.java
+++ sample/src/main/java/com/just/agentweb/sample/activity/CommonActivity.java
@@ -31,147 +31,150 @@ import static com.just.agentweb.sample.sonic.SonicJavaScriptInterface.PARAM_CLIC
 public class CommonActivity extends AppCompatActivity {
 
 
-    private FrameLayout mFrameLayout;
-    public static final String TYPE_KEY = "type_key";
-    private FragmentManager mFragmentManager;
+	private FrameLayout mFrameLayout;
+	public static final String TYPE_KEY = "type_key";
+	private FragmentManager mFragmentManager;
 
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+	@Override
+	protected void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_common);
+		setContentView(R.layout.activity_common);
 
-        mFrameLayout = (FrameLayout) this.findViewById(R.id.container_framelayout);
-
-
-        int key = getIntent().getIntExtra(TYPE_KEY, -1);
-        mFragmentManager = this.getSupportFragmentManager();
-        openFragment(key);
-    }
+		mFrameLayout = (FrameLayout) this.findViewById(R.id.container_framelayout);
 
 
-    private AgentWebFragment mAgentWebFragment;
-
-    private void openFragment(int key) {
-
-        FragmentTransaction ft = mFragmentManager.beginTransaction();
-        Bundle mBundle = null;
+		int key = getIntent().getIntExtra(TYPE_KEY, -1);
+		mFragmentManager = this.getSupportFragmentManager();
+		openFragment(key);
+	}
 
 
-        switch (key) {
+	private AgentWebFragment mAgentWebFragment;
+
+	private void openFragment(int key) {
+
+		FragmentTransaction ft = mFragmentManager.beginTransaction();
+		Bundle mBundle = null;
+
+
+		switch (key) {
 
             /*Fragment 使用AgenWeb*/
-            case 0: //请使用常量代替0 ， 代码可读性更高
-                ft.add(R.id.container_framelayout, mAgentWebFragment = AgentWebFragment.getInstance(mBundle = new Bundle()), AgentWebFragment.class.getName());
-                mBundle.putString(AgentWebFragment.URL_KEY, "https://m.vip.com/?source=www&jump_https=1");
-                break;
-            /*下载文件*/
-            case 1:
-                ft.add(R.id.container_framelayout, mAgentWebFragment = AgentWebFragment.getInstance(mBundle = new Bundle()), AgentWebFragment.class.getName());
-                mBundle.putString(AgentWebFragment.URL_KEY, "http://android.myapp.com/");
-                break;
+			case 0: //请使用常量代替0 ， 代码可读性更高
+				ft.add(R.id.container_framelayout, mAgentWebFragment = AgentWebFragment.getInstance(mBundle = new Bundle()), AgentWebFragment.class.getName());
+				mBundle.putString(AgentWebFragment.URL_KEY, "https://m.vip.com/?source=www&jump_https=1");
+				break;
+		    /*下载文件*/
+			case 1:
+				ft.add(R.id.container_framelayout, mAgentWebFragment = AgentWebFragment.getInstance(mBundle = new Bundle()), AgentWebFragment.class.getName());
+				mBundle.putString(AgentWebFragment.URL_KEY, "http://android.myapp.com/");
+				break;
             /*input标签上传文件*/
-            case 2:
-                ft.add(R.id.container_framelayout, mAgentWebFragment = AgentWebFragment.getInstance(mBundle = new Bundle()), AgentWebFragment.class.getName());
-                mBundle.putString(AgentWebFragment.URL_KEY, "file:///android_asset/upload_file/uploadfile.html");
-                break;
+			case 2:
+				ft.add(R.id.container_framelayout, mAgentWebFragment = AgentWebFragment.getInstance(mBundle = new Bundle()), AgentWebFragment.class.getName());
+				mBundle.putString(AgentWebFragment.URL_KEY, "file:///android_asset/upload_file/uploadfile.html");
+				break;
             /*Js上传文件*/
-            case 3:
-                ft.add(R.id.container_framelayout, mAgentWebFragment = AgentWebFragment.getInstance(mBundle = new Bundle()), AgentWebFragment.class.getName());
-                mBundle.putString(AgentWebFragment.URL_KEY, "file:///android_asset/upload_file/jsuploadfile.html");
-                break;
+			case 3:
+				ft.add(R.id.container_framelayout, mAgentWebFragment = AgentWebFragment.getInstance(mBundle = new Bundle()), AgentWebFragment.class.getName());
+				mBundle.putString(AgentWebFragment.URL_KEY, "file:///android_asset/upload_file/jsuploadfile.html");
+				break;
             /*Js*/
-            case 4:
-                ft.add(R.id.container_framelayout, mAgentWebFragment = JsAgentWebFragment.getInstance(mBundle = new Bundle()), JsAgentWebFragment.class.getName());
-                mBundle.putString(AgentWebFragment.URL_KEY, "file:///android_asset/js_interaction/hello.html");
-                break;
+			case 4:
+				ft.add(R.id.container_framelayout, mAgentWebFragment = JsAgentWebFragment.getInstance(mBundle = new Bundle()), JsAgentWebFragment.class.getName());
+				mBundle.putString(AgentWebFragment.URL_KEY, "file:///android_asset/js_interaction/hello.html");
+				break;
 
             /*优酷*/
-            case 5:
-                ft.add(R.id.container_framelayout, mAgentWebFragment = AgentWebFragment.getInstance(mBundle = new Bundle()), AgentWebFragment.class.getName());
-                mBundle.putString(AgentWebFragment.URL_KEY, "http://m.youku.com/video/id_XODEzMjU1MTI4.html");
+			case 5:
+				ft.add(R.id.container_framelayout, mAgentWebFragment = AgentWebFragment.getInstance(mBundle = new Bundle()), AgentWebFragment.class.getName());
+				mBundle.putString(AgentWebFragment.URL_KEY, "http://m.youku.com/video/id_XODEzMjU1MTI4.html");
 //                mBundle.putString(AgentWebFragment.URL_KEY, "https://v.qq.com/x/page/i0530nu6z1a.html");
-                break;
+				break;
             /*淘宝*/
-            case 6:
-                ft.add(R.id.container_framelayout, mAgentWebFragment = CustomIndicatorFragment.getInstance(mBundle = new Bundle()), CustomIndicatorFragment.class.getName());
-                mBundle.putString(AgentWebFragment.URL_KEY, "https://m.taobao.com/?sprefer=sypc00");
-                break;
+			case 6:
+				ft.add(R.id.container_framelayout, mAgentWebFragment = CustomIndicatorFragment.getInstance(mBundle = new Bundle()), CustomIndicatorFragment.class.getName());
+				mBundle.putString(AgentWebFragment.URL_KEY, "https://m.taobao.com/?sprefer=sypc00");
+				break;
             /*豌豆荚*/
-            case 7:
-                ft.add(R.id.container_framelayout, mAgentWebFragment = CustomSettingsFragment.getInstance(mBundle = new Bundle()), CustomSettingsFragment.class.getName());
-                mBundle.putString(AgentWebFragment.URL_KEY, "http://www.wandoujia.com/apps");
-                break;
+			case 7:
+				ft.add(R.id.container_framelayout, mAgentWebFragment = CustomSettingsFragment.getInstance(mBundle = new Bundle()), CustomSettingsFragment.class.getName());
+				mBundle.putString(AgentWebFragment.URL_KEY, "http://www.wandoujia.com/apps");
+				break;
 
             /*短信*/
-            case 8:
-                ft.add(R.id.container_framelayout, mAgentWebFragment = AgentWebFragment.getInstance(mBundle = new Bundle()), AgentWebFragment.class.getName());
-                mBundle.putString(AgentWebFragment.URL_KEY, "file:///android_asset/sms/sms.html");
-                break;
+			case 8:
+				ft.add(R.id.container_framelayout, mAgentWebFragment = AgentWebFragment.getInstance(mBundle = new Bundle()), AgentWebFragment.class.getName());
+				mBundle.putString(AgentWebFragment.URL_KEY, "file:///android_asset/sms/sms.html");
+				break;
             /* 自定义 WebView */
-            case 9:
-                ft.add(R.id.container_framelayout, mAgentWebFragment = CustomWebViewFragment.getInstance(mBundle = new Bundle()), CustomWebViewFragment.class.getName());
-                mBundle.putString(AgentWebFragment.URL_KEY, "");
-                break;
+			case 9:
+				ft.add(R.id.container_framelayout, mAgentWebFragment = CustomWebViewFragment.getInstance(mBundle = new Bundle()), CustomWebViewFragment.class.getName());
+				mBundle.putString(AgentWebFragment.URL_KEY, "");
+				break;
             /*回弹效果*/
-            case 10:
-                ft.add(R.id.container_framelayout, mAgentWebFragment = BounceWebFragment.getInstance(mBundle = new Bundle()), BounceWebFragment.class.getName());
-                mBundle.putString(AgentWebFragment.URL_KEY, "http://m.mogujie.com/?f=mgjlm&ptp=_qd._cps______3069826.152.1.0");
-                break;
+			case 10:
+				ft.add(R.id.container_framelayout, mAgentWebFragment = BounceWebFragment.getInstance(mBundle = new Bundle()), BounceWebFragment.class.getName());
+				mBundle.putString(AgentWebFragment.URL_KEY, "http://m.mogujie.com/?f=mgjlm&ptp=_qd._cps______3069826.152.1.0");
+				break;
 
             /*JsBridge 演示*/
-            case 11:
-                ft.add(R.id.container_framelayout, mAgentWebFragment = JsbridgeWebFragment.getInstance(mBundle = new Bundle()), JsbridgeWebFragment.class.getName());
-                mBundle.putString(AgentWebFragment.URL_KEY, "file:///android_asset/jsbridge/demo.html");
-                break;
+			case 11:
+				ft.add(R.id.container_framelayout, mAgentWebFragment = JsbridgeWebFragment.getInstance(mBundle = new Bundle()), JsbridgeWebFragment.class.getName());
+				mBundle.putString(AgentWebFragment.URL_KEY, "file:///android_asset/jsbridge/demo.html");
+				break;
 
             /*SmartRefresh 下拉刷新*/
-            case 12:
-                ft.add(R.id.container_framelayout, mAgentWebFragment = SmartRefreshWebFragment.getInstance(mBundle = new Bundle()), SmartRefreshWebFragment.class.getName());
-                mBundle.putString(AgentWebFragment.URL_KEY, "http://www.163.com/");
-                break;
+			case 12:
+				ft.add(R.id.container_framelayout, mAgentWebFragment = SmartRefreshWebFragment.getInstance(mBundle = new Bundle()), SmartRefreshWebFragment.class.getName());
+				mBundle.putString(AgentWebFragment.URL_KEY, "http://www.163.com/");
+				break;
                 /*地图*/
-            case 13:
-                ft.add(R.id.container_framelayout, mAgentWebFragment = AgentWebFragment.getInstance(mBundle = new Bundle()), AgentWebFragment.class.getName());
-                mBundle.putString(AgentWebFragment.URL_KEY, "https://map.baidu.com/mobile/webapp/index/index/#index/index/foo=bar/vt=map");
-                break;
+			case 13:
+				ft.add(R.id.container_framelayout, mAgentWebFragment = AgentWebFragment.getInstance(mBundle = new Bundle()), AgentWebFragment.class.getName());
+				mBundle.putString(AgentWebFragment.URL_KEY, "https://map.baidu.com/mobile/webapp/index/index/#index/index/foo=bar/vt=map");
+				break;
                 /*首屏秒开*/
-            case 14:
-                ft.add(R.id.container_framelayout, mAgentWebFragment = VasSonicFragment.create(mBundle = new Bundle()), AgentWebFragment.class.getName());
-                mBundle.putLong(PARAM_CLICK_TIME,getIntent().getLongExtra(PARAM_CLICK_TIME,-1L));
-                mBundle.putString(AgentWebFragment.URL_KEY, "http://mc.vip.qq.com/demo/indexv3");
-                break;
+			case 14:
+				ft.add(R.id.container_framelayout, mAgentWebFragment = VasSonicFragment.create(mBundle = new Bundle()), AgentWebFragment.class.getName());
+				mBundle.putLong(PARAM_CLICK_TIME, getIntent().getLongExtra(PARAM_CLICK_TIME, -1L));
+				mBundle.putString(AgentWebFragment.URL_KEY, "http://mc.vip.qq.com/demo/indexv3");
+				break;
+			default:
+				break;
 
-        }
-        ft.commit();
+		}
+		ft.commit();
 
-    }
+	}
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        //一定要保证 mAentWebFragemnt 回调
-        mAgentWebFragment.onActivityResult(requestCode, resultCode, data);
-    }
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		//一定要保证 mAentWebFragemnt 回调
+		mAgentWebFragment.onActivityResult(requestCode, resultCode, data);
+	}
 
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
 
-        AgentWebFragment mAgentWebFragment = this.mAgentWebFragment;
-        if (mAgentWebFragment != null) {
-            FragmentKeyDown mFragmentKeyDown = mAgentWebFragment;
-            if (mFragmentKeyDown.onFragmentKeyDown(keyCode, event))
-                return true;
-            else
-                return super.onKeyDown(keyCode, event);
-        }
+		AgentWebFragment mAgentWebFragment = this.mAgentWebFragment;
+		if (mAgentWebFragment != null) {
+			FragmentKeyDown mFragmentKeyDown = mAgentWebFragment;
+			if (mFragmentKeyDown.onFragmentKeyDown(keyCode, event)) {
+				return true;
+			} else {
+				return super.onKeyDown(keyCode, event);
+			}
+		}
 
-        return super.onKeyDown(keyCode, event);
-    }
+		return super.onKeyDown(keyCode, event);
+	}
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
 
-    }
+	}
 }
