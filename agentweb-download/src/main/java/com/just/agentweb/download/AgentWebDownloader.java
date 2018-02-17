d6a3f8067fe23a9c871094b4f64b09e7bf90b71b
Justson <xiaozhongcen@gmail.com>
2018-02-17 11:19:23 +0800

    #

--- agentweb-download/src/main/java/com/just/agentweb/download/AgentWebDownloader.java
+++ agentweb-download/src/main/java/com/just/agentweb/download/AgentWebDownloader.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C)  Justson(https://github.com/Justson/AgentWeb)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.just.agentweb.download;
 
 import android.support.annotation.DrawableRes;
@@ -5,9 +21,9 @@ import android.support.annotation.DrawableRes;
 import java.io.Serializable;
 
 /**
- * Created by cenxiaozhong on 2018/2/4.
+ * @author cenxiaozhong
+ * @date 2018/2/24
  */
-
 public interface AgentWebDownloader<T extends AgentWebDownloader.Extra> extends DownloadingService {
 
     void download(T t);
