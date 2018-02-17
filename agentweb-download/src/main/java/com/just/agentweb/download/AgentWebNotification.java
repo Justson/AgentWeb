281fd519eaf256792381ee383e8d35fd43448f21
Justson <xiaozhongcen@gmail.com>
2018-02-17 11:19:23 +0800

    #

--- agentweb-download/src/main/java/com/just/agentweb/download/AgentWebNotification.java
+++ agentweb-download/src/main/java/com/just/agentweb/download/AgentWebNotification.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C)  Justson(https://github.com/Justson/AgentWeb)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.just.agentweb.download;
 
 import android.app.Notification;
@@ -14,12 +30,10 @@ import com.just.agentweb.LogUtils;
 
 import static android.content.Context.NOTIFICATION_SERVICE;
 import static com.just.agentweb.AgentWebConfig.AGENTWEB_VERSION;
-
 /**
- * Created by cenxiaozhong on 2017/5/13.
- * https://github.com/Justson/AgentWeb
+ * @author cenxiaozhong
+ * @date 2018/5/13
  */
-
 public class AgentWebNotification {
 
     private static final int FLAG = Notification.FLAG_INSISTENT;
--- agentweb-download/src/main/java/com/just/agentweb/download/NotificationCancelReceiver.java
+++ agentweb-download/src/main/java/com/just/agentweb/download/NotificationCancelReceiver.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C)  Justson(https://github.com/Justson/AgentWeb)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.just.agentweb.download;
 
 import android.content.BroadcastReceiver;
@@ -5,11 +21,10 @@ import android.content.Context;
 import android.content.Intent;
 
 import com.just.agentweb.LogUtils;
-
 /**
- * Created by cenxiaozhong on 2018/2/12.
+ * @author cenxiaozhong
+ * @date 2018/2/12
  */
-
 public class NotificationCancelReceiver extends BroadcastReceiver {
 
 
