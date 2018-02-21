6bb1f3f8c5ec47715128e86f9444e441d332f67b
Justson <xiaozhongcen@gmail.com>
2018-02-21 17:57:03 +0800

    Update method Scope

--- agentweb-core/src/main/java/com/just/agentweb/IndicatorHandler.java
+++ agentweb-core/src/main/java/com/just/agentweb/IndicatorHandler.java
@@ -24,63 +24,65 @@ import android.webkit.WebView;
  * @since 1.0.0
  */
 public class IndicatorHandler implements IndicatorController {
-    private BaseIndicatorSpec mBaseIndicatorSpec;
+	private BaseIndicatorSpec mBaseIndicatorSpec;
 
-    @Override
-    public void progress(WebView v, int newProgress) {
+	@Override
+	public void progress(WebView v, int newProgress) {
 
-        if (newProgress == 0) {
-            reset();
-        } else if (newProgress > 0 && newProgress <= 10) {
-            showProgressBar();
-        } else if (newProgress > 10 && newProgress < 95) {
-            setProgress(newProgress);
-        } else {
-            setProgress(newProgress);
-            finish();
-        }
+		if (newProgress == 0) {
+			reset();
+		} else if (newProgress > 0 && newProgress <= 10) {
+			showIndicator();
+		} else if (newProgress > 10 && newProgress < 95) {
+			setProgress(newProgress);
+		} else {
+			setProgress(newProgress);
+			finish();
+		}
 
-    }
+	}
 
-    @Override
-    public BaseIndicatorSpec offerIndicator() {
-        return this.mBaseIndicatorSpec;
-    }
+	@Override
+	public BaseIndicatorSpec offerIndicator() {
+		return this.mBaseIndicatorSpec;
+	}
 
-    public void reset() {
+	public void reset() {
 
-        if (mBaseIndicatorSpec != null) {
-            mBaseIndicatorSpec.reset();
-        }
-    }
+		if (mBaseIndicatorSpec != null) {
+			mBaseIndicatorSpec.reset();
+		}
+	}
 
-    @Override
-    public void finish() {
-        if (mBaseIndicatorSpec != null) {
-            mBaseIndicatorSpec.hide();
-        }
-    }
-    @Override
-    public void setProgress(int n) {
-        if (mBaseIndicatorSpec != null) {
-            mBaseIndicatorSpec.setProgress(n);
-        }
-    }
-    @Override
-    public void showProgressBar() {
+	@Override
+	public void finish() {
+		if (mBaseIndicatorSpec != null) {
+			mBaseIndicatorSpec.hide();
+		}
+	}
 
-        if (mBaseIndicatorSpec != null) {
-            mBaseIndicatorSpec.show();
-        }
-    }
+	@Override
+	public void setProgress(int n) {
+		if (mBaseIndicatorSpec != null) {
+			mBaseIndicatorSpec.setProgress(n);
+		}
+	}
 
-    public static IndicatorHandler getInstance() {
-        return new IndicatorHandler();
-    }
+	@Override
+	public void showIndicator() {
+
+		if (mBaseIndicatorSpec != null) {
+			mBaseIndicatorSpec.show();
+		}
+	}
+
+	static IndicatorHandler getInstance() {
+		return new IndicatorHandler();
+	}
 
 
-    public IndicatorHandler inJectProgressView(BaseIndicatorSpec baseIndicatorSpec) {
-        this.mBaseIndicatorSpec = baseIndicatorSpec;
-        return this;
-    }
+	IndicatorHandler inJectProgressView(BaseIndicatorSpec baseIndicatorSpec) {
+		this.mBaseIndicatorSpec = baseIndicatorSpec;
+		return this;
+	}
 }
