2c4b402d49168813dfeb0377bf898197e30707a6
Justson <xiaozhongcen@gmail.com>
2018-02-24 10:05:50 +0800

    Add CoolIndicator Demo

--- sample/src/main/java/com/just/agentweb/sample/fragment/CustomIndicatorFragment.java
+++ sample/src/main/java/com/just/agentweb/sample/fragment/CustomIndicatorFragment.java
@@ -2,18 +2,14 @@ package com.just.agentweb.sample.fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
 import android.widget.LinearLayout;
-import android.widget.ProgressBar;
 
 import com.just.agentweb.AgentWeb;
 import com.just.agentweb.AgentWebSettingsImpl;
 import com.just.agentweb.DefaultWebClient;
-import com.just.agentweb.sample.R;
-import com.just.agentweb.sample.widget.CommonIndicator;
+import com.just.agentweb.sample.widget.CoolIndicatorLayout;
 
 /**
  * Created by cenxiaozhong on 2017/5/26.
@@ -31,18 +27,18 @@ public class CustomIndicatorFragment extends AgentWebFragment {
 	@Override
 	public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
 
-        CommonIndicator mCommonIndicator=new CommonIndicator(this.getActivity());
-        FrameLayout.LayoutParams lp=new FrameLayout.LayoutParams(-2,-2);
-        lp.gravity= Gravity.CENTER;
-        ProgressBar mProgressBar=new ProgressBar(this.getActivity());
-        mProgressBar.setBackground(this.getResources().getDrawable(R.drawable.indicator_shape));
-        mCommonIndicator.addView(mProgressBar,lp);
+//        CommonIndicator mCommonIndicator=new CommonIndicator(this.getActivity());
+//        FrameLayout.LayoutParams lp=new FrameLayout.LayoutParams(-2,-2);
+//        lp.gravity= Gravity.CENTER;
+//        ProgressBar mProgressBar=new ProgressBar(this.getActivity());
+//        mProgressBar.setBackground(this.getResources().getDrawable(R.drawable.indicator_shape));
+//        mCommonIndicator.addView(mProgressBar,lp);
 
 
-
+		CoolIndicatorLayout mCoolIndicatorLayout = new CoolIndicatorLayout(this.getActivity());
 		this.mAgentWeb = AgentWeb.with(this)//
 				.setAgentWebParent((ViewGroup) view, new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT))
-				.setCustomIndicator(mCommonIndicator)
+				.setCustomIndicator(mCoolIndicatorLayout)
 				.setAgentWebWebSettings(AgentWebSettingsImpl.getInstance())
 				.setWebViewClient(mWebViewClient)
 				.setPermissionInterceptor(mPermissionInterceptor)
