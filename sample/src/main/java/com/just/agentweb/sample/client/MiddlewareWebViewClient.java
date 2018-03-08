185bce0e3d3887ebdb4dd35670c539731e42a09f
Justson <xiaozhongcen@gmail.com>
2018-03-08 20:59:57 +0800

    docs(MiddlewareWebViewClient.java): explain middleware exec order

--- sample/src/main/java/com/just/agentweb/sample/client/MiddlewareWebViewClient.java
+++ sample/src/main/java/com/just/agentweb/sample/client/MiddlewareWebViewClient.java
@@ -8,6 +8,27 @@ import com.just.agentweb.MiddlewareWebClientBase;
 
 /**
  * Created by cenxiaozhong on 2017/12/16.
+ *
+ *
+ * 方法的执行顺序，例如下面用了7个中间件一个 WebViewClient
+ *
+ * .useMiddlewareWebClient(getMiddlewareWebClient())  // 1
+ * .useMiddlewareWebClient(getMiddlewareWebClient())  // 2
+ * .useMiddlewareWebClient(getMiddlewareWebClient())  // 3
+ * .useMiddlewareWebClient(getMiddlewareWebClient())  // 4
+ * .useMiddlewareWebClient(getMiddlewareWebClient())  // 5
+ * .useMiddlewareWebClient(getMiddlewareWebClient())  // 6
+ * .useMiddlewareWebClient(getMiddlewareWebClient())  // 7
+ *  DefaultWebClient                                  // 8
+ *  .setWebViewClient(mWebViewClient)                 // 9
+ *
+ *
+ * 典型的洋葱模型
+ * 对象内部的方法执行顺序: 1->2->3->4->5->6->7->8->9->8->7->6->5->4->3->2->1
+ *
+ *
+ * 中断中间件的执行， 删除super.methodName(...) 这行即可
+ *
  */
 
 public class MiddlewareWebViewClient extends MiddlewareWebClientBase {
