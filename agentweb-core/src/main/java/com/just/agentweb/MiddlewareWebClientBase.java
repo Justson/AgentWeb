6734998e5593236c9697df65ca093a4869d2174b
Justson <xiaozhongcen@gmail.com>
2018-06-23 23:17:31 +0800

    perf(MiddlewareWebClientBase.java): update TAG

--- agentweb-core/src/main/java/com/just/agentweb/MiddlewareWebClientBase.java
+++ agentweb-core/src/main/java/com/just/agentweb/MiddlewareWebClientBase.java
@@ -25,7 +25,7 @@ import android.webkit.WebViewClient;
  */
 public class MiddlewareWebClientBase extends WebViewClientDelegate {
     private MiddlewareWebClientBase mMiddleWrareWebClientBase;
-    private String TAG = this.getClass().getSimpleName();
+    private static String TAG = MiddlewareWebClientBase.class.getSimpleName();
 
     MiddlewareWebClientBase(MiddlewareWebClientBase client) {
         super(client);
