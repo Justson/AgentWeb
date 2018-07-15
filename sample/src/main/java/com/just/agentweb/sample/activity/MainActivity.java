a2e59fc75e5e493a2c8a75d549ed4fe6c2f14f6e
Justson <xiaozhongcen@gmail.com>
2018-07-15 10:15:11 +0800

    style(MainActivity.java): update sample

--- sample/src/main/java/com/just/agentweb/sample/activity/MainActivity.java
+++ sample/src/main/java/com/just/agentweb/sample/activity/MainActivity.java
@@ -37,42 +37,42 @@ public class MainActivity extends AppCompatActivity {
 	public static final int FLAG_GUIDE_DICTIONARY_USE_IN_FRAGMENT = FLAG_GUIDE_DICTIONARY_USE_IN_ACTIVITY << 1;
 	public static final int FLAG_GUIDE_DICTIONARY_FILE_DOWNLOAD = FLAG_GUIDE_DICTIONARY_USE_IN_FRAGMENT << 1;
 	public static final int FLAG_GUIDE_DICTIONARY_INPUT_TAG_PROBLEM = FLAG_GUIDE_DICTIONARY_FILE_DOWNLOAD << 1;
-	public static final int FLAG_GUIDE_DICTIONARY_JS_JAVA_COMMUNICATION=FLAG_GUIDE_DICTIONARY_INPUT_TAG_PROBLEM<<1;
-	public static final int FLAG_GUIDE_DICTIONARY_VIDEO_FULL_SCREEN=FLAG_GUIDE_DICTIONARY_JS_JAVA_COMMUNICATION<<1;
-	public static final int FLAG_GUIDE_DICTIONARY_CUSTOM_PROGRESSBAR=FLAG_GUIDE_DICTIONARY_VIDEO_FULL_SCREEN<<1;
-	public static final int FLAG_GUIDE_DICTIONARY_CUSTOM_WEBVIEW_SETTINGS=FLAG_GUIDE_DICTIONARY_CUSTOM_PROGRESSBAR<<1;
-	public static final int FLAG_GUIDE_DICTIONARY_LINKS=FLAG_GUIDE_DICTIONARY_CUSTOM_WEBVIEW_SETTINGS<<1;
-	public static final int FLAG_GUIDE_DICTIONARY_BOUNCE_EFFACT =FLAG_GUIDE_DICTIONARY_LINKS<<1;
-	public static final int FLAG_GUIDE_DICTIONARY_JSBRIDGE_SAMPLE= FLAG_GUIDE_DICTIONARY_BOUNCE_EFFACT <<1;
-	public static final int FLAG_GUIDE_DICTIONARY_EXTENDS_BASE_ACT=FLAG_GUIDE_DICTIONARY_JSBRIDGE_SAMPLE<<1;
-	public static final int FLAG_GUIDE_DICTIONARY_EXTENDS_BASE_FRAG=FLAG_GUIDE_DICTIONARY_EXTENDS_BASE_ACT<<1;
-	public static final int FLAG_GUIDE_DICTIONARY_PULL_DOWN_REFRESH=FLAG_GUIDE_DICTIONARY_EXTENDS_BASE_FRAG<<1;
-	public static final int FLAG_GUIDE_DICTIONARY_MAP=FLAG_GUIDE_DICTIONARY_PULL_DOWN_REFRESH<<1;
-	public static final int FLAG_GUIDE_DICTIONARY_VASSONIC_SAMPLE=FLAG_GUIDE_DICTIONARY_MAP<<1;
-	public static final int FLAG_GUIDE_DICTIONARY_LINKAGE_WITH_TOOLBAR =FLAG_GUIDE_DICTIONARY_VASSONIC_SAMPLE<<1;
-	public static final int FLAG_GUIDE_DICTIONARY_CUTSTOM_WEBVIEW= FLAG_GUIDE_DICTIONARY_LINKAGE_WITH_TOOLBAR <<1;
-	public static final int FLAG_GUIDE_DICTIONARY_JS_JAVA_COMUNICATION_UPLOAD_FILE=FLAG_GUIDE_DICTIONARY_CUTSTOM_WEBVIEW<<1;
+	public static final int FLAG_GUIDE_DICTIONARY_JS_JAVA_COMMUNICATION = FLAG_GUIDE_DICTIONARY_INPUT_TAG_PROBLEM << 1;
+	public static final int FLAG_GUIDE_DICTIONARY_VIDEO_FULL_SCREEN = FLAG_GUIDE_DICTIONARY_JS_JAVA_COMMUNICATION << 1;
+	public static final int FLAG_GUIDE_DICTIONARY_CUSTOM_PROGRESSBAR = FLAG_GUIDE_DICTIONARY_VIDEO_FULL_SCREEN << 1;
+	public static final int FLAG_GUIDE_DICTIONARY_CUSTOM_WEBVIEW_SETTINGS = FLAG_GUIDE_DICTIONARY_CUSTOM_PROGRESSBAR << 1;
+	public static final int FLAG_GUIDE_DICTIONARY_LINKS = FLAG_GUIDE_DICTIONARY_CUSTOM_WEBVIEW_SETTINGS << 1;
+	public static final int FLAG_GUIDE_DICTIONARY_BOUNCE_EFFACT = FLAG_GUIDE_DICTIONARY_LINKS << 1;
+	public static final int FLAG_GUIDE_DICTIONARY_JSBRIDGE_SAMPLE = FLAG_GUIDE_DICTIONARY_BOUNCE_EFFACT << 1;
+	public static final int FLAG_GUIDE_DICTIONARY_EXTENDS_BASE_ACT = FLAG_GUIDE_DICTIONARY_JSBRIDGE_SAMPLE << 1;
+	public static final int FLAG_GUIDE_DICTIONARY_EXTENDS_BASE_FRAG = FLAG_GUIDE_DICTIONARY_EXTENDS_BASE_ACT << 1;
+	public static final int FLAG_GUIDE_DICTIONARY_PULL_DOWN_REFRESH = FLAG_GUIDE_DICTIONARY_EXTENDS_BASE_FRAG << 1;
+	public static final int FLAG_GUIDE_DICTIONARY_MAP = FLAG_GUIDE_DICTIONARY_PULL_DOWN_REFRESH << 1;
+	public static final int FLAG_GUIDE_DICTIONARY_VASSONIC_SAMPLE = FLAG_GUIDE_DICTIONARY_MAP << 1;
+	public static final int FLAG_GUIDE_DICTIONARY_LINKAGE_WITH_TOOLBAR = FLAG_GUIDE_DICTIONARY_VASSONIC_SAMPLE << 1;
+	public static final int FLAG_GUIDE_DICTIONARY_CUTSTOM_WEBVIEW = FLAG_GUIDE_DICTIONARY_LINKAGE_WITH_TOOLBAR << 1;
+	public static final int FLAG_GUIDE_DICTIONARY_JS_JAVA_COMUNICATION_UPLOAD_FILE = FLAG_GUIDE_DICTIONARY_CUTSTOM_WEBVIEW << 1;
 	public static final GuideItemEntity[] datas = new GuideItemEntity[]{
 			new GuideItemEntity("Activity 使用 AgentWeb", FLAG_GUIDE_DICTIONARY_USE_IN_ACTIVITY),
 			new GuideItemEntity("Fragment 使用 AgentWeb ", FLAG_GUIDE_DICTIONARY_USE_IN_FRAGMENT),
 			new GuideItemEntity("文件下载", FLAG_GUIDE_DICTIONARY_FILE_DOWNLOAD),
-			new GuideItemEntity("input标签文件上传",FLAG_GUIDE_DICTIONARY_INPUT_TAG_PROBLEM),
-			new GuideItemEntity("Js 通信文件上传,兼用Android 4.4Kitkat",FLAG_GUIDE_DICTIONARY_JS_JAVA_COMUNICATION_UPLOAD_FILE),
-			new GuideItemEntity("Js 通信",FLAG_GUIDE_DICTIONARY_JS_JAVA_COMMUNICATION),
-			new GuideItemEntity("Video 视频全屏播放",FLAG_GUIDE_DICTIONARY_VIDEO_FULL_SCREEN),
-			new GuideItemEntity("自定义进度条",FLAG_GUIDE_DICTIONARY_CUSTOM_PROGRESSBAR),
-			new GuideItemEntity("自定义设置",FLAG_GUIDE_DICTIONARY_CUSTOM_WEBVIEW_SETTINGS),
-			new GuideItemEntity("电话 ， 信息 ， 邮件",FLAG_GUIDE_DICTIONARY_LINKS),
-			new GuideItemEntity("自定义 WebView",FLAG_GUIDE_DICTIONARY_CUTSTOM_WEBVIEW),
-			new GuideItemEntity("下拉回弹效果",FLAG_GUIDE_DICTIONARY_BOUNCE_EFFACT),
-			new GuideItemEntity("Jsbridge 例子",FLAG_GUIDE_DICTIONARY_JSBRIDGE_SAMPLE),
-			new GuideItemEntity("继承 BaseAgentWebActivity",FLAG_GUIDE_DICTIONARY_EXTENDS_BASE_ACT),
-			new GuideItemEntity("继承 BaseAgentWebFragment",FLAG_GUIDE_DICTIONARY_EXTENDS_BASE_FRAG),
-			new GuideItemEntity("SmartRefresh 下拉刷新",FLAG_GUIDE_DICTIONARY_PULL_DOWN_REFRESH),
-			new GuideItemEntity("地图",FLAG_GUIDE_DICTIONARY_MAP),
-			new GuideItemEntity("VasSonic 首屏秒开",FLAG_GUIDE_DICTIONARY_VASSONIC_SAMPLE),
+			new GuideItemEntity("input标签文件上传", FLAG_GUIDE_DICTIONARY_INPUT_TAG_PROBLEM),
+			new GuideItemEntity("Js 通信文件上传,兼用Android 4.4Kitkat", FLAG_GUIDE_DICTIONARY_JS_JAVA_COMUNICATION_UPLOAD_FILE),
+			new GuideItemEntity("Js 通信", FLAG_GUIDE_DICTIONARY_JS_JAVA_COMMUNICATION),
+			new GuideItemEntity("Video 视频全屏播放", FLAG_GUIDE_DICTIONARY_VIDEO_FULL_SCREEN),
+			new GuideItemEntity("自定义进度条", FLAG_GUIDE_DICTIONARY_CUSTOM_PROGRESSBAR),
+			new GuideItemEntity("自定义设置", FLAG_GUIDE_DICTIONARY_CUSTOM_WEBVIEW_SETTINGS),
+			new GuideItemEntity("电话 ， 信息 ， 邮件", FLAG_GUIDE_DICTIONARY_LINKS),
+			new GuideItemEntity("自定义 WebView", FLAG_GUIDE_DICTIONARY_CUTSTOM_WEBVIEW),
+			new GuideItemEntity("下拉回弹效果", FLAG_GUIDE_DICTIONARY_BOUNCE_EFFACT),
+			new GuideItemEntity("Jsbridge 例子", FLAG_GUIDE_DICTIONARY_JSBRIDGE_SAMPLE),
+			new GuideItemEntity("继承 BaseAgentWebActivity", FLAG_GUIDE_DICTIONARY_EXTENDS_BASE_ACT),
+			new GuideItemEntity("继承 BaseAgentWebFragment", FLAG_GUIDE_DICTIONARY_EXTENDS_BASE_FRAG),
+			new GuideItemEntity("SmartRefresh 下拉刷新", FLAG_GUIDE_DICTIONARY_PULL_DOWN_REFRESH),
+			new GuideItemEntity("地图", FLAG_GUIDE_DICTIONARY_MAP),
+			new GuideItemEntity("VasSonic 首屏秒开", FLAG_GUIDE_DICTIONARY_VASSONIC_SAMPLE),
 			new GuideItemEntity("与ToolBar联动", FLAG_GUIDE_DICTIONARY_LINKAGE_WITH_TOOLBAR),
-			};
+	};
 
 
 	@RequiresApi(api = Build.VERSION_CODES.KITKAT)
@@ -123,7 +123,6 @@ public class MainActivity extends AppCompatActivity {
 	}
 
 
-
 	private void doClick(int position) {
 
 
@@ -135,41 +134,53 @@ public class MainActivity extends AppCompatActivity {
 				startActivity(new Intent(this, WebActivity.class));
 				break;
 			case FLAG_GUIDE_DICTIONARY_USE_IN_FRAGMENT:
-				startActivity(new Intent(this, CommonActivity.class).putExtra(CommonActivity.TYPE_KEY, 0));
+				startActivity(new Intent(this, CommonActivity.class)
+						.putExtra(CommonActivity.TYPE_KEY, FLAG_GUIDE_DICTIONARY_USE_IN_FRAGMENT));
 				break;
 			case FLAG_GUIDE_DICTIONARY_FILE_DOWNLOAD:
-				startActivity(new Intent(this, CommonActivity.class).putExtra(CommonActivity.TYPE_KEY, 1));
+				startActivity(new Intent(this, CommonActivity.class)
+						.putExtra(CommonActivity.TYPE_KEY, FLAG_GUIDE_DICTIONARY_FILE_DOWNLOAD));
 				break;
 			case FLAG_GUIDE_DICTIONARY_INPUT_TAG_PROBLEM:
-				startActivity(new Intent(this, CommonActivity.class).putExtra(CommonActivity.TYPE_KEY, 2));
+				startActivity(new Intent(this, CommonActivity.class)
+						.putExtra(CommonActivity.TYPE_KEY, FLAG_GUIDE_DICTIONARY_INPUT_TAG_PROBLEM));
 				break;
 			case FLAG_GUIDE_DICTIONARY_JS_JAVA_COMUNICATION_UPLOAD_FILE:
-				startActivity(new Intent(this, CommonActivity.class).putExtra(CommonActivity.TYPE_KEY, 3));
+				startActivity(new Intent(this, CommonActivity.class)
+						.putExtra(CommonActivity.TYPE_KEY, FLAG_GUIDE_DICTIONARY_JS_JAVA_COMUNICATION_UPLOAD_FILE));
 				break;
 			case FLAG_GUIDE_DICTIONARY_JS_JAVA_COMMUNICATION:
-				startActivity(new Intent(this, CommonActivity.class).putExtra(CommonActivity.TYPE_KEY, 4));
+				startActivity(new Intent(this, CommonActivity.class)
+						.putExtra(CommonActivity.TYPE_KEY, FLAG_GUIDE_DICTIONARY_JS_JAVA_COMMUNICATION));
 				break;
 			case FLAG_GUIDE_DICTIONARY_VIDEO_FULL_SCREEN:
-				startActivity(new Intent(this, CommonActivity.class).putExtra(CommonActivity.TYPE_KEY, 5));
+				startActivity(new Intent(this, CommonActivity.class)
+						.putExtra(CommonActivity.TYPE_KEY, FLAG_GUIDE_DICTIONARY_VIDEO_FULL_SCREEN));
 				break;
 
 			case FLAG_GUIDE_DICTIONARY_CUSTOM_PROGRESSBAR:
-				startActivity(new Intent(this, CommonActivity.class).putExtra(CommonActivity.TYPE_KEY, 6));
+				startActivity(new Intent(this, CommonActivity.class)
+						.putExtra(CommonActivity.TYPE_KEY, FLAG_GUIDE_DICTIONARY_CUSTOM_PROGRESSBAR));
 				break;
 			case FLAG_GUIDE_DICTIONARY_CUSTOM_WEBVIEW_SETTINGS:
-				startActivity(new Intent(this, CommonActivity.class).putExtra(CommonActivity.TYPE_KEY, 7));
+				startActivity(new Intent(this, CommonActivity.class)
+						.putExtra(CommonActivity.TYPE_KEY, FLAG_GUIDE_DICTIONARY_CUSTOM_WEBVIEW_SETTINGS));
 				break;
 			case FLAG_GUIDE_DICTIONARY_LINKS:
-				startActivity(new Intent(this, CommonActivity.class).putExtra(CommonActivity.TYPE_KEY, 8));
+				startActivity(new Intent(this, CommonActivity.class)
+						.putExtra(CommonActivity.TYPE_KEY, FLAG_GUIDE_DICTIONARY_LINKS));
 				break;
 			case FLAG_GUIDE_DICTIONARY_CUTSTOM_WEBVIEW:
-				startActivity(new Intent(this, CommonActivity.class).putExtra(CommonActivity.TYPE_KEY, 9));
+				startActivity(new Intent(this, CommonActivity.class)
+						.putExtra(CommonActivity.TYPE_KEY, FLAG_GUIDE_DICTIONARY_CUTSTOM_WEBVIEW));
 				break;
 			case FLAG_GUIDE_DICTIONARY_BOUNCE_EFFACT:
-				startActivity(new Intent(this, CommonActivity.class).putExtra(CommonActivity.TYPE_KEY, 10));
+				startActivity(new Intent(this, CommonActivity.class)
+						.putExtra(CommonActivity.TYPE_KEY, FLAG_GUIDE_DICTIONARY_BOUNCE_EFFACT));
 				break;
 			case FLAG_GUIDE_DICTIONARY_JSBRIDGE_SAMPLE:
-				startActivity(new Intent(this, CommonActivity.class).putExtra(CommonActivity.TYPE_KEY, 11));
+				startActivity(new Intent(this, CommonActivity.class)
+						.putExtra(CommonActivity.TYPE_KEY, FLAG_GUIDE_DICTIONARY_JSBRIDGE_SAMPLE));
 				break;
 			case FLAG_GUIDE_DICTIONARY_EXTENDS_BASE_ACT:
 				startActivity(new Intent(this, EasyWebActivity.class));
@@ -180,13 +191,17 @@ public class MainActivity extends AppCompatActivity {
 				break;
 
 			case FLAG_GUIDE_DICTIONARY_PULL_DOWN_REFRESH:
-				startActivity(new Intent(this, CommonActivity.class).putExtra(CommonActivity.TYPE_KEY, 12));
+				startActivity(new Intent(this, CommonActivity.class)
+						.putExtra(CommonActivity.TYPE_KEY, FLAG_GUIDE_DICTIONARY_PULL_DOWN_REFRESH));
 				break;
 			case FLAG_GUIDE_DICTIONARY_MAP:
-				startActivity(new Intent(this, CommonActivity.class).putExtra(CommonActivity.TYPE_KEY, 13));
+				startActivity(new Intent(this, CommonActivity.class)
+						.putExtra(CommonActivity.TYPE_KEY, FLAG_GUIDE_DICTIONARY_MAP));
 				break;
 			case FLAG_GUIDE_DICTIONARY_VASSONIC_SAMPLE:
-				startActivity(new Intent(this, CommonActivity.class).putExtra(CommonActivity.TYPE_KEY, 14).putExtra(PARAM_CLICK_TIME, System.currentTimeMillis()));
+				startActivity(new Intent(this,
+						CommonActivity.class).putExtra(CommonActivity.TYPE_KEY, FLAG_GUIDE_DICTIONARY_VASSONIC_SAMPLE)
+						.putExtra(PARAM_CLICK_TIME, System.currentTimeMillis()));
 				break;
 			case FLAG_GUIDE_DICTIONARY_LINKAGE_WITH_TOOLBAR:
 				startActivity(new Intent(this, AutoHidenToolbarActivity.class));
