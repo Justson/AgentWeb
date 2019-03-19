00a625d57a50d291e0162da1974afdabc60f01a2
Justson <xiaozhongcen@gmail.com>
2019-03-19 15:46:04 +0800

    refactor(agentweb-core): update url loader , support for specified URLs and corresponding headers

--- agentweb-core/src/main/java/com/just/agentweb/IUrlLoader.java
+++ agentweb-core/src/main/java/com/just/agentweb/IUrlLoader.java
@@ -27,20 +27,20 @@ import java.util.Map;
 public interface IUrlLoader {
 
 
-	void loadUrl(String url);
+    void loadUrl(String url);
 
-	void loadUrl(String url, Map<String, String> headers);
+    void loadUrl(String url, Map<String, String> headers);
 
-	void reload();
+    void reload();
 
-	void loadData(String data, String mimeType, String encoding);
+    void loadData(String data, String mimeType, String encoding);
 
-	void stopLoading();
+    void stopLoading();
 
-	void loadDataWithBaseURL(String baseUrl, String data,
-	                         String mimeType, String encoding, String historyUrl);
+    void loadDataWithBaseURL(String baseUrl, String data,
+                             String mimeType, String encoding, String historyUrl);
 
-	void postUrl(String url, byte[] params);
+    void postUrl(String url, byte[] params);
 
-	HttpHeaders getHttpHeaders();
+    HttpHeaders getHttpHeaders();
 }
--- agentweb-core/src/main/java/com/just/agentweb/UrlLoaderImpl.java
+++ agentweb-core/src/main/java/com/just/agentweb/UrlLoaderImpl.java
@@ -27,131 +27,134 @@ import java.util.Map;
  * @since 2.0.0
  */
 public class UrlLoaderImpl implements IUrlLoader {
-	private Handler mHandler = null;
-	private WebView mWebView;
-	private HttpHeaders mHttpHeaders;
+    private Handler mHandler = null;
+    private WebView mWebView;
+    private HttpHeaders mHttpHeaders;
 
-	UrlLoaderImpl(WebView webView, HttpHeaders httpHeaders) {
-		this.mWebView = webView;
-		if (this.mWebView == null) {
-			new NullPointerException("webview cannot be null .");
-		}
-		this.mHttpHeaders = httpHeaders;
-		mHandler = new Handler(Looper.getMainLooper());
-	}
+    UrlLoaderImpl(WebView webView, HttpHeaders httpHeaders) {
+        this.mWebView = webView;
+        if (this.mWebView == null) {
+            new NullPointerException("webview cannot be null .");
+        }
+        this.mHttpHeaders = httpHeaders;
+        if (this.mHttpHeaders == null) {
+            this.mHttpHeaders = HttpHeaders.create();
+        }
+        mHandler = new Handler(Looper.getMainLooper());
+    }
 
-	private void safeLoadUrl(final String url) {
-		mHandler.post(new Runnable() {
-			@Override
-			public void run() {
-				loadUrl(url);
-			}
-		});
-	}
+    private void safeLoadUrl(final String url) {
+        mHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                loadUrl(url);
+            }
+        });
+    }
 
-	private void safeReload() {
-		mHandler.post(new Runnable() {
-			@Override
-			public void run() {
-				reload();
-			}
-		});
-	}
+    private void safeReload() {
+        mHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                reload();
+            }
+        });
+    }
 
-	@Override
-	public void loadUrl(String url) {
-		this.loadUrl(url, null);
-	}
+    @Override
+    public void loadUrl(String url) {
+        this.loadUrl(url, this.mHttpHeaders.getHeaders(url));
+    }
 
-	@Override
-	public void loadUrl(final String url, final Map<String, String> headers) {
-		if (!AgentWebUtils.isUIThread()) {
-			AgentWebUtils.runInUiThread(new Runnable() {
-				@Override
-				public void run() {
-					loadUrl(url, headers);
-				}
-			});
-		}
-		if (headers == null || headers.isEmpty()) {
-			this.mWebView.loadUrl(url);
-		} else {
-			this.mWebView.loadUrl(url, headers);
-		}
-	}
+    @Override
+    public void loadUrl(final String url, final Map<String, String> headers) {
+        if (!AgentWebUtils.isUIThread()) {
+            AgentWebUtils.runInUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    loadUrl(url, headers);
+                }
+            });
+        }
+        if (headers == null || headers.isEmpty()) {
+            this.mWebView.loadUrl(url);
+        } else {
+            this.mWebView.loadUrl(url, headers);
+        }
+    }
 
-	@Override
-	public void reload() {
-		if (!AgentWebUtils.isUIThread()) {
-			mHandler.post(new Runnable() {
-				@Override
-				public void run() {
-					reload();
-				}
-			});
-			return;
-		}
-		this.mWebView.reload();
-	}
+    @Override
+    public void reload() {
+        if (!AgentWebUtils.isUIThread()) {
+            mHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    reload();
+                }
+            });
+            return;
+        }
+        this.mWebView.reload();
+    }
 
-	@Override
-	public void loadData(final String data, final String mimeType, final String encoding) {
-		if (!AgentWebUtils.isUIThread()) {
-			mHandler.post(new Runnable() {
-				@Override
-				public void run() {
-					loadData(data, mimeType, encoding);
-				}
-			});
-			return;
-		}
-		this.mWebView.loadData(data, mimeType, encoding);
-	}
+    @Override
+    public void loadData(final String data, final String mimeType, final String encoding) {
+        if (!AgentWebUtils.isUIThread()) {
+            mHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    loadData(data, mimeType, encoding);
+                }
+            });
+            return;
+        }
+        this.mWebView.loadData(data, mimeType, encoding);
+    }
 
-	@Override
-	public void stopLoading() {
-		if (!AgentWebUtils.isUIThread()) {
-			mHandler.post(new Runnable() {
-				@Override
-				public void run() {
-					stopLoading();
-				}
-			});
-			return;
-		}
-		this.mWebView.stopLoading();
-	}
+    @Override
+    public void stopLoading() {
+        if (!AgentWebUtils.isUIThread()) {
+            mHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    stopLoading();
+                }
+            });
+            return;
+        }
+        this.mWebView.stopLoading();
+    }
 
-	@Override
-	public void loadDataWithBaseURL(final String baseUrl, final String data, final String mimeType, final String encoding, final String historyUrl) {
-		if (!AgentWebUtils.isUIThread()) {
-			mHandler.post(new Runnable() {
-				@Override
-				public void run() {
-					loadDataWithBaseURL(baseUrl, data, mimeType, encoding, historyUrl);
-				}
-			});
-			return;
-		}
-		this.mWebView.loadDataWithBaseURL(baseUrl, data, mimeType, encoding, historyUrl);
-	}
+    @Override
+    public void loadDataWithBaseURL(final String baseUrl, final String data, final String mimeType, final String encoding, final String historyUrl) {
+        if (!AgentWebUtils.isUIThread()) {
+            mHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    loadDataWithBaseURL(baseUrl, data, mimeType, encoding, historyUrl);
+                }
+            });
+            return;
+        }
+        this.mWebView.loadDataWithBaseURL(baseUrl, data, mimeType, encoding, historyUrl);
+    }
 
-	@Override
-	public void postUrl(final String url, final byte[] postData) {
-		if (!AgentWebUtils.isUIThread()) {
-			mHandler.post(new Runnable() {
-				@Override
-				public void run() {
-					postUrl(url, postData);
-				}
-			});
-			return;
-		}
-		this.mWebView.postUrl(url, postData);
-	}
+    @Override
+    public void postUrl(final String url, final byte[] postData) {
+        if (!AgentWebUtils.isUIThread()) {
+            mHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    postUrl(url, postData);
+                }
+            });
+            return;
+        }
+        this.mWebView.postUrl(url, postData);
+    }
 
-	@Override
-	public HttpHeaders getHttpHeaders() {
-		return this.mHttpHeaders == null ? this.mHttpHeaders = HttpHeaders.create() : this.mHttpHeaders;
-	}
+    @Override
+    public HttpHeaders getHttpHeaders() {
+        return this.mHttpHeaders == null ? this.mHttpHeaders = HttpHeaders.create() : this.mHttpHeaders;
+    }
 }
