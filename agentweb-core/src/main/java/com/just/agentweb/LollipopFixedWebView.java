b0ffb02cc38fa2e817abd2c49e89d72295ecfac6
liyujiang <1032694760@qq.com>
2019-10-12 15:25:52 +0800

    Fix #636 and #646

---
+++ agentweb-core/src/main/java/com/just/agentweb/LollipopFixedWebView.java
@@ -0,0 +1,45 @@
+package com.just.agentweb;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.res.Configuration;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.webkit.WebView;
+
+/**
+ * 修复 Android 5.0 & 5.1 打开 WebView 闪退问题：
+ * 参阅 https://stackoverflow.com/questions/41025200/android-view-inflateexception-error-inflating-class-android-webkit-webview
+ */
+@SuppressWarnings("unused")
+public class LollipopFixedWebView extends WebView {
+    public LollipopFixedWebView(Context context) {
+        super(getFixedContext(context));
+    }
+
+    public LollipopFixedWebView(Context context, AttributeSet attrs) {
+        super(getFixedContext(context), attrs);
+    }
+
+    public LollipopFixedWebView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(getFixedContext(context), attrs, defStyleAttr);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public LollipopFixedWebView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(getFixedContext(context), attrs, defStyleAttr, defStyleRes);
+    }
+
+    public LollipopFixedWebView(Context context, AttributeSet attrs, int defStyleAttr, boolean privateBrowsing) {
+        super(getFixedContext(context), attrs, defStyleAttr, privateBrowsing);
+    }
+
+    public static Context getFixedContext(Context context) {
+        if (Build.VERSION.SDK_INT >= 21 && Build.VERSION.SDK_INT < 23) {
+            // Avoid crashing on Android 5 and 6 (API level 21 to 23)
+            return context.createConfigurationContext(new Configuration());
+        }
+        return context;
+    }
+
+}
