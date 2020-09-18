0853f47bbf3335d66b35bd485e82a61cf17da237
Justson <xiaozhongcen@gmail.com>
2020-09-18 18:33:26 +0800

    fix(agentweb): fix sample jsbridge error

--- sample/src/main/java/com/just/agentweb/sample/fragment/JsbridgeWebFragment.java
+++ sample/src/main/java/com/just/agentweb/sample/fragment/JsbridgeWebFragment.java
@@ -1,11 +1,13 @@
 package com.just.agentweb.sample.fragment;
 
 import android.graphics.Bitmap;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.widget.LinearLayout;
 
@@ -17,6 +19,8 @@ import com.google.gson.Gson;
 import com.just.agentweb.AgentWeb;
 import com.just.agentweb.WebViewClient;
 
+
+
 /**
  * Created by cenxiaozhong on 2017/7/1.
  * source code  https://github.com/Justson/AgentWeb
@@ -24,99 +28,108 @@ import com.just.agentweb.WebViewClient;
 
 public class JsbridgeWebFragment extends AgentWebFragment {
 
-	public static JsbridgeWebFragment getInstance(Bundle bundle) {
+    public static JsbridgeWebFragment getInstance(Bundle bundle) {
 
-		JsbridgeWebFragment mJsbridgeWebFragment = new JsbridgeWebFragment();
-		if (mJsbridgeWebFragment != null) {
-			mJsbridgeWebFragment.setArguments(bundle);
-		}
+        JsbridgeWebFragment mJsbridgeWebFragment = new JsbridgeWebFragment();
+        if (mJsbridgeWebFragment != null) {
+            mJsbridgeWebFragment.setArguments(bundle);
+        }
 
-		return mJsbridgeWebFragment;
-	}
+        return mJsbridgeWebFragment;
+    }
 
-	private BridgeWebView mBridgeWebView;
+    private BridgeWebView mBridgeWebView;
 
-	@Override
-	public String getUrl() {
-		return super.getUrl();
-	}
+    @Override
+    public String getUrl() {
+        return super.getUrl();
+    }
 
-	@Override
-	public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
 
-		mBridgeWebView = new BridgeWebView(getActivity());
-		mAgentWeb = AgentWeb.with(this)
-				.setAgentWebParent((ViewGroup) view, new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT))
-				.useDefaultIndicator(-1, 2)
-				.setAgentWebWebSettings(getSettings())
-				.setWebViewClient(getWebViewClient())
-				.setWebChromeClient(mWebChromeClient)
-				.setWebView(mBridgeWebView)
-				.setSecurityType(AgentWeb.SecurityType.STRICT_CHECK)
+        mBridgeWebView = new BridgeWebView(getActivity());
+        mAgentWeb = AgentWeb.with(this)
+                .setAgentWebParent((ViewGroup) view, new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT))
+                .useDefaultIndicator(-1, 2)
+                .setAgentWebWebSettings(getSettings())
+                .setWebViewClient(getWebViewClient())
+                .setWebChromeClient(mWebChromeClient)
+                .setWebView(mBridgeWebView)
+                .setSecurityType(AgentWeb.SecurityType.STRICT_CHECK)
 //                .setDownloadListener(mDownloadListener) 4.0.0 删除该API
-				.createAgentWeb()//
-				.ready()//
-				.go(getUrl());
+                .createAgentWeb()//
+                .ready()//
+                .go(getUrl());
 
 
-		initView(view);
+        initView(view);
 
 
-		mBridgeWebView.registerHandler("submitFromWeb", new BridgeHandler() {
+        mBridgeWebView.registerHandler("submitFromWeb", new BridgeHandler() {
 
-			@Override
-			public void handler(String data, CallBackFunction function) {
-				function.onCallBack("submitFromWeb exe, response data 中文 from Java");
-			}
+            @Override
+            public void handler(String data, CallBackFunction function) {
+                function.onCallBack("submitFromWeb exe, response data 中文 from Java");
+            }
 
-		});
+        });
 
-		User user = new User();
-		Location location = new Location();
-		location.address = "SDU";
-		user.location = location;
-		user.name = "Agentweb --> Jsbridge";
-		mBridgeWebView.callHandler("functionInJs", new Gson().toJson(user), new CallBackFunction() {
-			@Override
-			public void onCallBack(String data) {
-				Log.i(TAG, "data:" + data);
-			}
-		});
+        User user = new User();
+        Location location = new Location();
+        location.address = "SDU";
+        user.location = location;
+        user.name = "Agentweb --> Jsbridge";
+        mBridgeWebView.callHandler("functionInJs", new Gson().toJson(user), new CallBackFunction() {
+            @Override
+            public void onCallBack(String data) {
+                Log.i(TAG, "data:" + data);
+            }
+        });
 
-		mBridgeWebView.send("hello");
-	}
+        mBridgeWebView.send("hello");
+    }
 
-	private WebViewClient getWebViewClient(){
-		return new WebViewClient() {
-			BridgeWebViewClient mBridgeWebViewClient = new BridgeWebViewClient(mBridgeWebView);
+    private WebViewClient getWebViewClient() {
+        return new WebViewClient() {
+            BridgeWebViewClient mBridgeWebViewClient = new BridgeWebViewClient(mBridgeWebView);
 
-			@Override
-			public boolean shouldOverrideUrlLoading(WebView view, String url) {
-				return mBridgeWebViewClient.shouldOverrideUrlLoading(view, url);
-			}
-
-			@Override
-			public void onPageStarted(WebView view, String url, Bitmap favicon) {
-				super.onPageStarted(view, url, favicon);
-			}
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                return mBridgeWebViewClient.shouldOverrideUrlLoading(view, url);
+            }
 
 			@Override
-			public void onPageFinished(WebView view, String url) {
-				super.onPageFinished(view, url);
-				mBridgeWebViewClient.onPageFinished(view, url);
-			}
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                if (mBridgeWebViewClient.shouldOverrideUrlLoading(view, request.getUrl().toString())) {
+                	return true;
+				}else{
+					return super.shouldOverrideUrlLoading(view, request);
+				}
+            }
 
-		};
-	}
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+            }
 
-	static class Location {
-		String address;
-	}
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                mBridgeWebViewClient.onPageFinished(view, url);
+            }
 
-	static class User {
-		String name;
-		Location location;
-		String testStr;
-	}
+        };
+    }
+
+    static class Location {
+        String address;
+    }
+
+    static class User {
+        String name;
+        Location location;
+        String testStr;
+    }
 
 }
